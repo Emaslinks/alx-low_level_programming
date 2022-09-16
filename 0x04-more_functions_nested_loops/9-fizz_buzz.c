abe17fa1ec91a61301938da79fb837bd6327170d
emaslinks <ayobamiemmanuel13@gmail.com>
2022-09-16 05:47:19 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>                                                                                                                                   
+#include  <stdio.h>                                                                                                                                   
 #include <stdlib.h>                                                                                                                                  
 /**                                                                                                                                                  
  * main - prints the numbers from 1 to 100                                                                                                           
@@ -9,23 +9,24 @@
  */                                                                                                                                                  
 int main(void)                                                                                                                                       
 {                                                                                                                                                    
-	int i;                                                                                                                                               
-	char f[] = "Fizz";                                                                                                                                   
-	char b[] = "Buzz";                                                                                                                                   
-	char fb[] = "FizzBuzz";                                                                                                                              
-	for (i = 1; i <= 100; i++)                                                                                                                           
-	{                                                                                                                                                    
-	if (i == 100)                                                                                                                                        
-		printf("%s", b);                                                                                                                                     
-	else if ((i % 3 == 0) && (i % 5 == 0))                                                                                                               
-		printf("%s ", fb);                                                                                                                                   
-	else if (i % 3 == 0)                                                                                                                                 
-		printf("%s ", f);                                                                                                                                    
-	else if (i % 5 == 0)                                                                                                                                 
-		printf("%s ", b);                                                                                                                                    
-	else                                                                                                                                                 
-		printf("%d ", i);                                                                                                                                    
-	}                                                                                                                                                    
-	printf("\n");                                                                                                                                        
-	return (0);                                                                                                                                          
+int i;                                                                                                                                               
+char f[] = "Fizz";                                                                                                                                   
+char b[] = "Buzz";                                                                                                                                   
+char fb[] = "FizzBuzz";                                                                                                                              
+for (i = 1; i <= 100; i++)                                                                                                                           
+{                                                                                                                                                    
+if (i == 100)                                                                                                                                        
+printf("%s", b);                                                                                                                                     
+else if ((i % 3 == 0) && (i % 5 == 0))                                                                                                               
+printf("%s ", fb);                                                                                                                                   
+else if (i % 3 == 0)                                                                                                                                 
+printf("%s ", f);                                                                                                                                    
+else if (i % 5 == 0)                                                                                                                                 
+printf("%s ", b);                                                                                                                                    
+else                                                                                                                                                 
+printf("%d ", i);                                                                                                                                    
+}                                                                                                                                                    
+printf("\n");                                                                                                                                        
+return (0);                                                                                                                                          
 }
+
