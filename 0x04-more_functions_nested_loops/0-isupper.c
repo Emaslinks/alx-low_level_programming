237652a8eb7dbf9dc32af9907ed2b28d4b57686f
emaslinks <ayobamiemmanuel13@gmail.com>
2022-09-16 01:57:58 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,13 +5,14 @@
  * @c: character to be tested
  * Return: 1 if uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A') && (c <= 'Z')
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
-	else
-		return(0);
+	
+	return(0);
 }
 
