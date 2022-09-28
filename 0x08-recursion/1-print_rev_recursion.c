736dd8b79bf108f4d1381c6c9659dc773134348d
emaslinks <ayobamiemmanuel13@gmail.com>
2022-09-28 15:35:56 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,14 @@
  */
 void _print_rev_recursion(char *s)
 {
-	s = "lpa\0";
 
-	if (*s == '\0')
-	    return;
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
+
+	if (*s != '\0')
+	{
+
+		
+	 	 _print_rev_recursion(s + 1);
+	 	 _putchar(*s);
+	}
+
 }
