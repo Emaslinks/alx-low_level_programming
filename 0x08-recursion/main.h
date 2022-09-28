dc5789178aa25da70289959c8cf0984c4ece6156
emaslinks <ayobamiemmanuel13@gmail.com>
2022-09-28 15:10:16 -0700

    Task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,10 +10,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-in _sqrt(int n int x);
-int is_prime(int n, int start);
 int is_palindrome(char *s);
-void check(char *s, int start, int end, int *flag);
 int wildcmp(char *s1, char *s2);
 
 #endif /*MAIN_H*/
