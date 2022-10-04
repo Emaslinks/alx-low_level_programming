8801abb629820ab2f1570bc3b838b721173a33d4
emaslinks <ayobamiemmanuel13@gmail.com>
2022-10-04 15:46:02 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,61 @@
 #include "main.h"
 
 /**
+ * _strlen - count array
+ * @s: array of elements
+ * Reurns: 1
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * str_concat - func that cooncatenates two strings
- * @s1; 1st string
- * @s2: 2nd string
+ * @s1; array one
+ * @s2: array two
  *
  * * Return: NULL in case of failure, but pointer to new strinf in
  * case of success
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *concat_str;
-        int index, concat_index = 0, len 0;
-        if (s1 == NULL)
-                s1 = "";
+	char *dst;
+	unsigned int i, j, size;
 
-        if (s2 == NULL)
-                s2 = "";
-        for (index = 0; s1[index] || s2[index]; index++)
-                len++;
-	concat_str = malloc(sizeof(char) * len);
-	if (concat_str == NULL)
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL0
+		s2 = "";
+	size = (_strlen(s1) + _strlen(s2) + 1);
+
+	dst = (char *) malloc(size * sizeof(char));
+
+	if (dst == 0)
+	{
 		return (NULL);
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
+	}
 
-	return (concat_str);
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(dst + i) = *(s1 + i);
+
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(dst + 1) = *(s2 + j);
+	}
+
+	return (dst);
+
+
 }
 
 
